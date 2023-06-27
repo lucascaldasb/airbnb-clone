8bf35dfbee97fdb3856cd1c4f570e303b30c2899
lucascaldasb <lucas20caldas@gmail.com>
2023-06-27 08:33:27 -0300

    adding search modal, loading and error page and a middleware to protect the routers if user isnt loged in
    Signed-off-by: lucascaldasb <lucas20caldas@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import ToasterProvider from './providers/ToasterProvider'
 import { LoginModal } from './components/Modals/LoginModal'
 import { getCurrentUser } from './actions/getCurrentUser'
 import { RentModal } from './components/Modals/RentModal'
+import { SearchModal } from './components/Modals/SearchModal'
 
 const nunito = Nunito({ subsets: ['latin'] })
 
@@ -27,6 +28,7 @@ export default async function RootLayout({
       <body className={nunito.className}>
         <ClientOnly>
           <ToasterProvider />
+          <SearchModal />
           <RentModal />
           <LoginModal />
           <RegisterModal />
